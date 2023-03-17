7ab81fcbcdca44adb052df2df55d10ba19b84e89
IKON3R <operezz2100@alumno.ipn.mx>
2023-03-16 23:18:18 -0600

    Instrucciones del juego

--- VideoJuego2/JS/letrasani.js
+++ VideoJuego2/JS/letrasani.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typed',{
-    strings: [' ', 'Hola Jugador', 'Bienvenido a', '<NOMBRE DEL JUEGO>', 'ESPERO SEA DIFICIL:)'],
+    strings: [' ', 'Hola Jugador', 'Bienvenido a', 'DPZN', 'ESPERO SEA DIFICIL:)'],
     typeSpeed: 75, //tiempo para para poner letras
     startDelay: 1000, //tiempo de retraso para iniciar
     backDelay: 800, //Velocidad para borrar
